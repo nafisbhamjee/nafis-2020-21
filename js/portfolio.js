558fbf4300e10c191f4a518c8c9df23cd2d74cb0
nafisbhamjee <P17157542@my365.dmu.ac.uk>
2021-04-22 15:20:13 +0100

    Javascript for slider control added

---
+++ js/portfolio.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+displaySlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  displaySlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  displaySlides(slideIndex = n);
+}
+
+function displaySlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
